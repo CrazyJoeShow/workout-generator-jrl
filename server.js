10c32d8d2e32b6e8ff40a82f4132b88138495aea
Joe Lowery <joseph.loweryit@gmail.com>
2020-10-23 14:27:46 -0400

    added changes

--- server.js
+++ server.js
@@ -1,46 +1,85 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./workout-gen/models");
+const app = express();
 
-const app = express()
 const PORT = process.env.PORT || 3000;
+const workoutsController = require("./workout-gen/controllers/workoutsController");
 
-app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-// mongoose.conncect(process.envmongoURI HERE) THEN chain parser,topology,index,and findModifyHERE
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect
+  (process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
     useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
     useFindAndModify: false,
-});
+  }
+);
 
-app.get("/", ({body}, res) => {
-    res.send("this may or may not be working??")
-});
-
-// connections for success error and connection HW 
 const connection = mongoose.connection;
-connection.on("connnected", () => {
-    console.log("Mongoose IS LIVE");
-});
+
+connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+})
+
 connection.on("error", (err) => {
-    console.log("Error connectiong to mongoose");
+    console.log("Mongoose connection error: ", err);
 });
+
+app.use(workoutsController);
+
 app.get("/api/config", (req, res) => {
-    res.json({ success: "Workout is connected"});
+    res.json({
+        success: true,
+    });
 });
 
-
-require(".routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-app.listen(PORT, () =>{
-    console.log(`Listening on port ${PORT}`);
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then((foundWorkouts) => {
+        res.json(foundWorkouts);
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to retrieve Workouts.",
+        });
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}!`);
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7).then((foundWorkoutRange) => {
+        res.json(foundWorkoutRange);
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to retrieve Workouts.",
+        });
+    });
 });
 
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body).then((newWorkout) => {
+        res.json(newWorkout);
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to create workout.",
+        });
+    });
+});
+
+app.listen(PORT,() => {
+    console.log(`App is running on http://localhost:${PORT}`);
+});
