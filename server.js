2f6ba64351a97912f0b95ec5be81097391fdcb21
Joe Lowery <joseph.loweryit@gmail.com>
2020-10-22 20:38:09 -0400

    folder structure change added index and workoutJS

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const mongoose = require("mongoose")
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get("/", ({body}, res) => {
+    res.send("this may or may not be working??")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}!`);
 });
